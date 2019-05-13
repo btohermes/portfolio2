dd9297ef73995ef632305a8cc22bd8ad054d924e
hermesbto <hermesbto.dev@gmail.com>
2019-05-13 15:33:49 +0100

    Added Responsiveness

--- src/containers/MacTerminal/MacTerminal.js
+++ src/containers/MacTerminal/MacTerminal.js
@@ -5,15 +5,11 @@ import classes from './MacTerminal.css';
 class MacTerminal extends Component {
   date = new Date().toString();
 
-  handleKeyPress = event => {
-    console.log('Hello');
-  };
-
   render() {
     return (
       <Fragment>
-        <div className={classes.MacTerminal} onKeyPress={this.handleKeyPress}>
-          <div className={classes.MacTerminal__head}>
+        <div className={classes.MacTerminal}>
+          <div tabIndex={-1} className={classes.MacTerminal__head}>
             <div className={classes.dotRed} />
             <div className={classes.dotYellow} />
             <div className={classes.dotGreen} />
@@ -22,7 +18,7 @@ class MacTerminal extends Component {
             <div>Last login:{this.date}</div>
             <div>Hello world!</div>
             <div>My name is Hermes. I am a software developer</div>
-            <div>Please Press any key to continue</div>
+            <div>Please press any key to continue</div>
             <div>
               ><span className={classes.flicker}>_</span>
             </div>
